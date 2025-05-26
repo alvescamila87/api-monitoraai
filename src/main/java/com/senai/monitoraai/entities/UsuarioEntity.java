4050412b47533cb6e6feb3dc66e78b8c12174080
CAMILA ALVES <alvescamila87@gmail.com>
2025-05-26 19:47:17 -0300

    feat: add model

---
+++ src/main/java/com/senai/monitoraai/entities/UsuarioEntity.java
@@ -0,0 +1,29 @@
+package com.senai.monitoraai.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+}
