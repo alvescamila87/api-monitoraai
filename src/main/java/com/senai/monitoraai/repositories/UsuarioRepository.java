90dadc319e6f1aeb2c358c20728af9a41be21115
CAMILA ALVES <alvescamila87@gmail.com>
2025-05-26 19:51:11 -0300

    feat: add repository

---
+++ src/main/java/com/senai/monitoraai/repositories/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.senai.monitoraai.repositories;
+
+import com.senai.monitoraai.entities.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+
+    Optional<UsuarioEntity> findByEmail (String email);
+}
