94a38d1b3a1f465ff1b366010ed48ddc2aba54b0
CAMILA ALVES <alvescamila87@gmail.com>
2025-05-26 19:22:24 -0300

    feat: start project SA

---
+++ src/main/java/com/senai/monitoraai/MonitoraaiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.monitoraai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoraaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoraaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/monitoraai/MonitoraaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.monitoraai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoraaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
