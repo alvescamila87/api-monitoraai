4e3754e2b362506e5150a9bccd5c1aaa0d4ba751
emidiojoao <joaojoaocarlos045@gmail.com>
2025-06-06 19:39:32 -0300

    feat: validadados service

--- src/main/java/com/senai/monitoraai/services/EmprestimoService.java
+++ src/main/java/com/senai/monitoraai/services/EmprestimoService.java
@@ -1,19 +1,32 @@
 package com.senai.monitoraai.services;
 
 import com.senai.monitoraai.dtos.emprestimo.EmprestimoListaDTO;
+import com.senai.monitoraai.dtos.emprestimo.EmprestimoRequestDTO;
+import com.senai.monitoraai.entities.ColaboradorEntity;
 import com.senai.monitoraai.entities.EmprestimoEntity;
+import com.senai.monitoraai.entities.EquipamentoEntity;
+import com.senai.monitoraai.exceptions.InvalidOperationException;
+import com.senai.monitoraai.repositories.ColaboradorRepository;
 import com.senai.monitoraai.repositories.EmprestimoRepository;
+import com.senai.monitoraai.repositories.EquipamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmprestimoService {
 
     @Autowired
     EmprestimoRepository repository;
+
+    @Autowired
+    ColaboradorRepository colaboradorRepository;
+
+    @Autowired
+    EquipamentoRepository equipamentoRepository;
 
     public List<EmprestimoListaDTO> listaEmprestimo() {
         List<EmprestimoListaDTO> listaEmprestimosDTO = new ArrayList<>();
@@ -25,6 +38,19 @@ public class EmprestimoService {
         }
 
         return listaEmprestimosDTO;
+    }
+
+    protected void validaDados(EmprestimoRequestDTO emprestimoRequestDTO){
+
+        Optional<ColaboradorEntity> colaboradorEntityOptional = colaboradorRepository.findById(emprestimoRequestDTO.getColaboradorId());
+        if(colaboradorEntityOptional.isEmpty()){
+            throw new InvalidOperationException("Colaborador não encontrado.");
+        }
+
+        Optional<EquipamentoEntity> equipamentoEntityOptional = equipamentoRepository.findById(emprestimoRequestDTO.getEquipamentoId());
+        if(equipamentoEntityOptional.isEmpty()){
+            throw new InvalidOperationException("Equipamento não encontrado.");
+        }
     }
 
 }
